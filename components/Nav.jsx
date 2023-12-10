a6664b2b51595b776729d9b5d62afbb26c0bea8b
Joe Kariuki <jknguru6@gmail.com>
2023-12-10 17:18:14 +0300

    Update nav to use auth session

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,17 +6,18 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     };
+    setUpProviders();
   }, []);
 
   return (
@@ -32,8 +33,9 @@ const Nav = () => {
         <p className="logo_text">Promptomania</p>
       </Link>
 
+      {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -60,7 +62,7 @@ const Nav = () => {
                   type="button"
                   key={provider.name}
                   onClick={() => signIn(provider.id)}
-                  className="black=_btn"
+                  className="black_btn"
                 >
                   Sign In
                 </button>
@@ -71,7 +73,7 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/logo.svg"
@@ -120,7 +122,7 @@ const Nav = () => {
                   type="button"
                   key={provider.name}
                   onClick={() => signIn(provider.id)}
-                  className="black=_btn"
+                  className="black_btn"
                 >
                   Sign In
                 </button>
