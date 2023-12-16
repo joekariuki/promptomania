feac1346a98f10e69f0ef1dab47864c0975aebad
Joe Kariuki <jknguru6@gmail.com>
2023-12-16 07:00:08 +0300

    Fix create prompt to include routing and user session

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,20 +2,24 @@
 
 import { useState } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({ prompt: "", tag: "" });
+
   const createPrompt = async (e) => {
     e.preventDefault();
     setSubmitting(true);
 
     try {
       const response = await fetch("/api/prompt/new", {
-        method: POST,
+        method: "POST",
         body: JSON.stringify({
           prompt: post.prompt,
           userId: session?.user.id,
